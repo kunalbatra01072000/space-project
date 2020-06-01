2585dcd47e3a6cf3c3c31241c35e3dfcc55f2d34
kunalbatra01072000 <kunalbatra01072000@gmail.com>
2020-06-02 00:00:40 +0530

    adding build

---
+++ build/precache-manifest.969318b3ca17ac4fe6f07a01e1bae420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de5301772e31535bbee282e4bb43bda",
+    "url": "/space-project/index.html"
+  },
+  {
+    "revision": "59911124f6c8dfcee5cb",
+    "url": "/space-project/static/css/main.afba6f3a.chunk.css"
+  },
+  {
+    "revision": "13d5b16f8b2e05c986bb",
+    "url": "/space-project/static/js/2.62c389fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space-project/static/js/2.62c389fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59911124f6c8dfcee5cb",
+    "url": "/space-project/static/js/main.7a5417b0.chunk.js"
+  },
+  {
+    "revision": "e01e3419b47bde15605b",
+    "url": "/space-project/static/js/runtime-main.abbd5921.js"
+  }
+]);
