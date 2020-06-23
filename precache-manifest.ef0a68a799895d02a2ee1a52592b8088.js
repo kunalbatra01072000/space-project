eefaf369a9268cce4b075a9fb65cc57ae24c4393
kunalbatra01072000 <kunalbatra01072000@gmail.com>
2020-06-23 09:07:27 +0530

    Updates

--- precache-manifest.ef0a68a799895d02a2ee1a52592b8088.js
+++ precache-manifest.ef0a68a799895d02a2ee1a52592b8088.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "586888017551c12b1a7d82f451e3de04",
+    "revision": "f9ef90f08d1a6c7b9a1fdbaeb48670b9",
     "url": "/space-project/index.html"
   },
   {
-    "revision": "c2ea6b585e6e4d2203e7",
+    "revision": "ec775a2c25e009d3b39b",
     "url": "/space-project/static/css/main.0e454d7d.chunk.css"
   },
   {
-    "revision": "897ecd6e5a7be03cc541",
-    "url": "/space-project/static/js/2.acca31ce.chunk.js"
+    "revision": "7cea4fbbfea1b3273b9a",
+    "url": "/space-project/static/js/2.cff64138.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/space-project/static/js/2.acca31ce.chunk.js.LICENSE.txt"
+    "url": "/space-project/static/js/2.cff64138.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2ea6b585e6e4d2203e7",
-    "url": "/space-project/static/js/main.349aa7f0.chunk.js"
+    "revision": "ec775a2c25e009d3b39b",
+    "url": "/space-project/static/js/main.9d70f4cf.chunk.js"
   },
   {
     "revision": "e01e3419b47bde15605b",
